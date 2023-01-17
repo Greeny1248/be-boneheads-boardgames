b81c4007312142268ace297274defc52b74a45c3
Josh <greeny1248@gmail.com>
2023-01-17 15:09:39 +0000

    Review for Q5 done

--- app.js
+++ app.js
@@ -5,16 +5,33 @@ const {
   viewAllCategories,
   viewAllReviews,
   viewReviewById,
+  viewCommentsFromReview,
 } = require("./controller/controller");
 
 app.get("/api/categories", viewAllCategories);
 app.get("/api/reviews", viewAllReviews);
 app.get("/api/reviews/:review_id", viewReviewById);
+app.get("/api/reviews/:review_id/comments", viewCommentsFromReview);
+
+app.all("/*", (req, res, next) => {
+  res.status(404).send({ msg: "Path not found" });
+});
+app.use((err, req, res, next) => {
+  if (err.status === 400) {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
+});
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  res.status(500).send({ msg: "Internal server error" });
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad request" });
+  } else {
+    next(err);
+  }
 });
+
 app.use((err, req, res, next) => {
   if (err.status && err.msg) {
     res.status(err.status).send({ msg: err.msg });
@@ -22,9 +39,9 @@ app.use((err, req, res, next) => {
     next(err);
   }
 });
-app.use((req, res, next) => {
-  res.status(404).send({ msg: "Route Does Not Exist" });
-  next(err);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send({ msg: "Internal server error" });
 });
 
 module.exports = app;
--- controller/controller.js
+++ controller/controller.js
@@ -3,6 +3,7 @@ const {
   readCategories,
   readReviews,
   fetchReviewById,
+  fetchCommentsFromReview,
 } = require("../model/model.js");
 
 const viewAllCategories = (req, res) => {
@@ -15,24 +16,40 @@ const viewAllCategories = (req, res) => {
     });
 };
 
-const viewAllReviews = (req, res) => {
+const viewAllReviews = (req, res, next) => {
   readReviews()
     .then((reviews) => {
       res.status(200).send({ reviews });
     })
     .catch((err) => {
       console.log(err);
+      next(err);
     });
 };
 
 const viewReviewById = (req, res, next) => {
   const { review_id } = req.params;
-  console.log(req.params, "Req");
   fetchReviewById(review_id)
     .then((review) => {
       res.status(200).send({ review });
     })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+};
+const viewCommentsFromReview = (req, res, next) => {
+  const { review_id } = req.params;
+  fetchCommentsFromReview(review_id)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
     .catch(next);
 };
 
-module.exports = { viewAllCategories, viewAllReviews, viewReviewById };
+module.exports = {
+  viewAllCategories,
+  viewAllReviews,
+  viewReviewById,
+  viewCommentsFromReview,
+};
