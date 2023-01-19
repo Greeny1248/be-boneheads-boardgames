2a6db56b28a712121a448f13573d154397904c7b
Josh <greeny1248@gmail.com>
2023-01-19 15:55:05 +0000

    Retest of hosting, updated package.json

--- controller/controller.js
+++ controller/controller.js
@@ -104,16 +104,6 @@ const deleteCommentById = (req, res, next) => {
     .catch(next);
 };
 
-const deleteCommentById = (req, res, next) => {
-  const { comment_id } = req.params;
-  removeCommentById(comment_id)
-    .then(() => {
-      res.status(204).send();
-    })
-    .catch(next);
-};
-
-
 module.exports = {
   viewJSON,
   viewAllCategories,
